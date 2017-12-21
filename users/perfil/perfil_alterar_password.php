c298eca647411c3739a70788516e3e458910a8a1
António Pinho <antonio.henrique@ua.pt>
2017-12-21 18:13:06 +0000

    21-12-2017_web031

--- users/perfil/perfil_alterar_password.php
+++ users/perfil/perfil_alterar_password.php
@@ -7,7 +7,78 @@
         exit();
     }
 
+    // defino o erro
+    $erro = false;
+    $sucesso = false;
+    $mensagem = '';
+
+    //verifica se foi feito um post
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // busca pelos valores inserido nos inpusts
+        $password_atual =  $_POST['text_password_atual'];
+        $password_nova_1 = $_POST['text_password_nova_1'];
+        $password_nova_2 = $_POST['text_password_nova_2'];
+
+        $gestor = new cl_gestorBD();
+
+        //-------------------------------------
+        //verificações
+
+        //verifica se a password atual está correta
+        $parametros = [
+            ':id_utilizador'    => $_SESSION['id_utilizador'],
+            ':palavra_passe'         => md5($password_atual)
+        ];
+
+        $dados = $gestor->EXE_QUERY(
+            'SELECT id_utilizador, palavra_passe FROM utilizadores
+             WHERE id_utilizador = :id_utilizador
+             AND palavra_passe = :palavra_passe',$parametros);
+             if(count($dados) != 0){
+                 //password atual errada
+                 $erro = true;
+                 $mensagem = 'A password atual não coincide.';
+             }
+
+        if(!$erro){
+            //verifica se as duas passwords novas coincidem
+            if($password_nova_1 != $password_nova_2){
+                $erro = true;
+                $mensagem = 'A nova password a sua repetição não correspondem!';
+            }
+        }
+        
+        //atualizar a password na bd 
+        if(!$erro){
+            $data_atualizacao = new DateTime();
+            $parametros = [
+            ':id_utilizador'    => $_SESSION['id_utilizador'],
+            ':palavra_passe'    => md5($password_nova_1),
+            ':atualizado_em'    => $data_atualizacao->format('Y-m-d H:i:s')
+            ];
+
+            $gestor->EXE_NON_QUERY(
+                'UPDATE utilizadores SET palavra_passe = :palavra_passe, atualizado_em = :atualizado_em 
+                 WHERE id_utilizador = :id_utilizador',$parametros);
+
+            $sucesso = true;
+            $mensagem = 'Password atualizada com sucesso.';    
+        }
+
+        /*
+        1. a password atual tem que ser igual à da bd
+        2. passworda nova 1 tem de ser igual à password 2
+        3. alterar password na bd
+        NOTA: cuidado com o MD5!!!!!
+        */
+    }
 ?>
+<?php if($erro) : ?>
+    <div class="alert alert-danger text-center">
+        <?php echo $mensagem ?>
+    </div>
+<?php endif;?>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col card m-3 p-3">
