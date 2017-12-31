741b6f676f988594e708850350f2407ed9083798
António Pinho <antonio.henrique@ua.pt>
2017-12-31 13:11:05 +0000

    31-12-2017_web041

--- admin/utilizadores_adicionar.php
+++ admin/utilizadores_adicionar.php
@@ -27,31 +27,83 @@
 
                 <!-- Formulário para adicionar novo utilizador -->
                 <form action="?a=utilizador_adicionar" method="post">
-                    <!-- Nome -->
+                    <!-- Útilizador -->
                     <div class="form-group">
-                        <label for="textutilizador">Utilizador:</label>
-                        <input id="textutilizador" type="text" name="text_utilizador" class="form-control" pattern=".{3,50}" title="Entre 3 a 50 caracteres." required>                 
+                        <label for="text_utilizador">Utilizador:</label>
+                        <input id="text_utilizador" 
+                               placeholder="UserName"   
+                               type="text" 
+                               name="text_utilizador" 
+                               class="form-control" 
+                               pattern=".{3,50}" 
+                               title="Entre 3 a 50 caracteres." 
+                               required>                 
                     </div>
 
                      <!-- password -->
                      <div class="form-group">
                         <label for="text_password">Password:</label>
-
                         <div class="row">
                             <div class="col"> 
-                                <input id="text_password" placeholder="Password" type="text" class="form-control" name="text_password" class="form-control" pattern=".{3,30}" title="Entre 3 a 50 caracteres." required>                
+                                <input id="text_password" 
+                                       placeholder="Password" 
+                                       type="text" 
+                                       class="form-control" 
+                                       name="text_password" 
+                                       class="form-control" 
+                                       pattern=".{3,30}" 
+                                       title="Entre 3 a 50 caracteres." 
+                                       required>                
                             </div>
                              <div class="col">
                                 <button type="button" class="btn btn-primary form-control" onclick="gerarPassword(10)">Gerar Password</button> 
                             </div>
                         </div>
                     </div>
-                        
-                </form>
-
-             <div class="text-center">
-                <a href="?a=utilizadores_gerir" class="btn btn-primary btn-size-150">Voltar</a>
-            </div>                  
+                      <!-- Nome Completo -->
+                    <div class="form-group">
+                        <label for="text_nome">Nome:</label>
+                        <input id="text_nome" 
+                               placeholder="Nome completo" 
+                               type="text" 
+                               name="text_nome" 
+                               class="form-control" 
+                               pattern=".{3,50}" 
+                               title="Entre 3 a 50 caracteres." 
+                               required>                 
+                    </div  >
+                    <!-- Email -->
+                    <div class="form-group">
+                        <label for="text_email">E-mail:</label>
+                        <input id="text_email" 
+                               placeholder="exemple@gmail.com" 
+                               type="email" 
+                               name="text_email" 
+                               class="form-control" 
+                               pattern=".{3,50}" 
+                               title="Entre 3 a 50 caracteres." 
+                               required>                 
+                    </div>                        
+                    <div class="text-center">
+                        <a href="?a=utilizadores_gerir" class="btn btn-primary btn-size-150">Calcelar</a>
+                        <button class="btn btn-primary btn-size-150">Criar útilizador</button>
+                    </div>
+                    <hr>
+                    <div class="text-center">
+                        <button type="button" 
+                                class="btn btn-primary btn-size-150" 
+                                data-toggle="collapse" 
+                                data-target="#caixa_permissoes" 
+                                aria-expanded="false" 
+                                aria-controls="collapseExample">Definir permissões</button>
+                        <div class="collapse" id="caixa_permissoes">
+                            <div class="card p-3">
+                                <!-- Permissões -->
+                                <p>Texto dentro da caixa collapse</p>
+                            </div>
+                        </div>                  
+                    </div>               
+                </form>                 
         </div>
     </div>
 </div>
