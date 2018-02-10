3a6ed8f382655eaead90ef837b497360b717159e
António Pinho <antonio.henrique@ua.pt>
2018-02-10 00:03:38 +0000

    10-02-2018_web063

--- _cabecalho.php
+++ _cabecalho.php
@@ -14,11 +14,18 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="inc/main.css">
+    <link rel="stylesheet" href="inc/main_web.css">
   </head>
   <body>
-    <div class="container-fluid cabecalho text-center">
-      <p>Cabeçalho da página web</p>
+
+    <!-- Barra do cliente -->
+    <div class="container-fluid barra-cliente">
+      <div class="text-right"><span>Login | Signup</span></div>
+    </div>
+
+    <!-- Cabeçalho -->
+    <div class="container-fluid cabecalho">
+     <img src="images/logo.png" alt="logo">
 
     </div>
   
