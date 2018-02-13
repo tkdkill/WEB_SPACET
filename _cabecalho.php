bcc526fbecb54204e4e80832d95fe74d6403efc8
António Pinho <antonio.henrique@ua.pt>
2018-02-13 16:00:58 +0000

    13-02-2018_web064

--- _cabecalho.php
+++ _cabecalho.php
@@ -14,18 +14,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="inc/main_web.css">
+    <!-- <link rel="stylesheet" href="inc/css/FontAwesome/fontawesome.min.css"> -->
+    <!-- <link href="inc/css/fontAwesome/fontawesome-all.css" rel="stylesheet"> -->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="inc/css/main_web.css">
   </head>
   <body>
 
     <!-- Barra do cliente -->
     <div class="container-fluid barra-cliente">
-      <div class="text-right"><span>Login | Signup</span></div>
+      <div class="text-right"><span><a href="http://"><i class="fas fa-sign-in-alt"> Login</i></a> | <a href="http://"> <i class="far fa-registered"> Signup</i></a></span></div>
     </div>
 
     <!-- Cabeçalho -->
     <div class="container-fluid cabecalho">
      <img src="images/logo.png" alt="logo">
-
     </div>
   
